04410132ee6de2d8844c93aca64034ad418efa7e
Liam <urehelonn@gmail.com>
2019-07-16 16:44:38 -0400

    ngform set, with form value listen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
+
+
+// routes value
+const appRoutes: Routes = [
+  { path: './signup', component: SignupComponent },
+  { path: '**', component: SignupComponent },
+  ]
 
 @NgModule({
   declarations: [
@@ -12,7 +22,12 @@ import { SignupComponent } from './signup/signup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      // { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
