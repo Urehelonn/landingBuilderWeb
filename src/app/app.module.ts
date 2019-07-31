b69edcc6c092ae794342904c0959b78dc4ad5196
zlan <lanzhuoqin@gmail.com>
2019-07-30 20:41:16 -0400

    homework setup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import {RouterModule, Routes} from '@angular/router';
 import {FormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
-import {SignupComponent} from './signup/signup.component';
-import {UserService} from './services/user.service';
-import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './features/auth/signup/signup.component';
+import {UserService} from './features/auth/user.service';
+import {LoginComponent} from './features/auth/login/login.component';
 import {HttpClientModule} from '@angular/common/http';
-import {ProfileEditComponent} from './profile/profile-edit/profile-edit.component';
-import {NavComponent} from './nav/nav.component';
-import {FooterComponent} from './footer/footer.component';
-import {NotFoundComponent} from './not-found/not-found.component';
-import {ProcessBarComponent} from './process-bar/process-bar.component';
-import {HomeComponent} from './home/home.component';
-import {ProfileDetailComponent} from './profile/profile-detail/profile-detail.component';
+import {ProfileEditComponent} from './features/profile/profile-edit/profile-edit.component';
+import {NavComponent} from './features/nav/nav.component';
+import {FooterComponent} from './features/footer/footer.component';
+import {NotFoundComponent} from './features/others/not-found/not-found.component';
+import {ProcessBarComponent} from './features/others/process-bar/process-bar.component';
+import {HomeComponent} from './features/home/home.component';
+import {ProfileDetailComponent} from './features/profile/profile-detail/profile-detail.component';
 import {AuthGuard} from "./core/auth.guard";
+import { BuilderComponent } from './features/builder/builder.component';
+import { HeadComponent } from './features/builder/head/head.component';
+import { GalleryComponent } from './features/builder/gallery/gallery.component';
+import { MenuComponent } from './features/builder/menu/menu.component';
 
 
 // routes value
@@ -29,6 +33,7 @@ const appRoutes: Routes = [
     {path: 'signup', component: SignupComponent},
     {path: 'login', component: LoginComponent},
     {path: 'profile', component: ProfileEditComponent},
+    {path: 'builder', component: BuilderComponent},
     {path: '**', component: NotFoundComponent},
 ];
 
@@ -44,6 +49,10 @@ const appRoutes: Routes = [
         ProcessBarComponent,
         HomeComponent,
         ProfileDetailComponent,
+        BuilderComponent,
+        HeadComponent,
+        GalleryComponent,
+        MenuComponent,
     ],
     imports: [
         BrowserModule,
--- src/app/features/profile/profile-edit/profile-edit.component.ts
+++ src/app/features/profile/profile-edit/profile-edit.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {UserService} from '../../services/user.service';
-import {AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
 import {Router} from "@angular/router";
+import {UserService} from "../../auth/user.service";
 
 @Component({
     selector: 'app-profile',
