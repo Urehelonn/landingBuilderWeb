4cbac037e311972f20fdd5e7a4fe8bd38b21792c
Jiangqi <jiangqi319@gmail.com>
2019-08-03 11:34:15 -0400

    update menu

--- src/app/features/builder/menu/menu.component.ts
+++ src/app/features/builder/menu/menu.component.ts
@@ -7,31 +7,49 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  @Input() menuData: any;
+  @Input() menuData;
+  categoryActive: boolean[] = [];
 
-  keyArray = [];
+  showAllMenuItem = true;
+
+  allItem: any[] = [];
 
   constructor() {
   }
 
   ngOnInit() {
-    for (var key in this.menuData.items) {
-      console.log("Key: " + key);
-      console.log("Value: " + this.menuData.items[key]);
-      this.keyArray.push(key);
-      console.log("Array : " + this.keyArray);
-    }
+    console.log(this.menuData);
+
+    //  initialize all menu category to false
+    this.menuData.dynamicMenu.forEach(menu => {
+      this.categoryActive.push(false);
+    });
+
+    this.getAllItem();
   }
 
-  getKey() {
-    for (var key in this.menuData.items) {
-      this.keyArray.push(key);
-      console.log("Array : " + this.keyArray);
-      // for (var i=0 ; i<this.menuData.items.length;i++) {
-      //   this.keyArray[i] = key[i];
-      //   console.log(this.keyArray);
-      // }
-    }
+  changeActiveMenuWithTag(tag: string) {
+    this.showAllMenuItem = false;
+    let count = 0;
+    this.menuData.dynamicMenu.forEach(cate => {
+      if (tag === cate.categoryName) {
+        this.categoryActive[count] = true;
+      } else {
+        this.categoryActive[count] = false;
+      }
+      count++;
+    });
   }
 
+  showAllMenuItemTrigger() {
+    this.showAllMenuItem = true;
+  }
+
+  private getAllItem() {
+    this.menuData.dynamicMenu.forEach(cate => {
+      cate.menuItem.forEach(item => {
+        this.allItem.push(item);
+      });
+    });
+  }
 }
